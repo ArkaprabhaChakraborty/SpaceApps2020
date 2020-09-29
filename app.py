92d07b7894db5aa4f7ca89c475273385f36d4fe5
arkaprabha <chakrabortyarkaprabha998@gmail.com>
2020-09-29 19:57:19 +0530

    Second comms

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ key = "HkyaRKrAG08v0FLeSPwUp7JR6rAEKVA6FO5vqZ8o"
 rev = requests.get("https://api.nasa.gov/planetary/apod?api_key="+key)
 rev = rev.json()
 image = rev['url']
+i = "https://www.google.com/earth/assets/static/images/overview/overview__get-started__mobile-image.jpg"
+if (image.find('youtube')):
+    image=i
 ex = rev['explanation']
 app = Flask(__name__)
 @app.route("/",methods = ['GET','POST'])
@@ -22,5 +25,7 @@ def Neo():
 @app.route("/Neo1")
 def neo_one():
     return render_template("Neo1.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
